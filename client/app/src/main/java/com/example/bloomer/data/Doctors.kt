4ac56c9eb637ebf662e04431e6ef63af57121fef
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2024-03-17 12:53:47 +0530

    Added doctor features

---
+++ client/app/src/main/java/com/example/bloomer/data/Doctors.kt
@@ -0,0 +1,8 @@
+package com.example.bloomer.data
+
+data class Doctors(
+    val id: String = "",
+    val name : String = "",
+    val address : String = "",
+    val mobile : String = ""
+)
--- client/app/src/main/java/com/example/bloomer/screens/AppointmentScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bloomer.screens
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun AppointmentScreen() {
-
-}
