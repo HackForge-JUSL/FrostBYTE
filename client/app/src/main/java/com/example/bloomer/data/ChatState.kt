0d73826de2231a5d11a05972656c5c006fe5e4b2
DeathKrow <irfanhgazi@gmail.com>
2024-03-17 12:26:10 +0530

    Created ViewModels for ChatScreen and ChatBot

---
+++ client/app/src/main/java/com/example/bloomer/data/ChatState.kt
@@ -0,0 +1,11 @@
+package com.example.bloomer.data
+
+import android.graphics.Bitmap
+import com.example.bloomer.data.Chat
+
+
+data class ChatState (
+    val chatList: MutableList<Chat> = mutableListOf(),
+    val prompt: String = "",
+    val bitmap: Bitmap? = null
+)
